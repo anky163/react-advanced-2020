c998c5dbe89023940501fa77a0450ff34925ed67
anky163 <nguyenducanky96@gmail.com>
2022-12-01 22:26:49 +0700

    AppContext

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ import React from 'react'
 // import Setup from './projects/useReducer-and-useContext/stripe-menus'
 // import Setup from './projects/useReducer-and-useContext/cart'
 
-import Setup from './projects/useCallback-useMemo/cocktail/index'
+import Setup from './projects/useCallback-memo/cocktail/index'
 
 
 
